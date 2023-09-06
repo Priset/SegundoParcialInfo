cc02efdfa9c2f341acb3d2860a48ac7fd51599d6
Priset <emanuel.1kll@gmail.com>
2023-09-05 23:12:35 -0400

    Details

---
+++ scripts/bottom_ui.gd
@@ -0,0 +1,7 @@
+extends TextureRect
+
+@onready var goal_label = $MarginContainer/HBoxContainer/goal_label
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
